cd038e912f1fd4896465af637d53ab12ee3c0937
Ren Guidolin <8.renji@gmail.com>
2022-01-31 17:25:05 -0500

    question

--- question.rb
+++ question.rb
@@ -1,5 +1,20 @@
 require './game'
 
 class Question 
-  
+  attr_accessor :answer, :test_question
+
+  def initialize 
+    @answer = nil # answer is initially nothing
+  end
+
+  def test_question
+    num = Random.new # Built into ruby - Random.new()
+
+    first_num = num.rand(100)
+    second_num = num.rand(100)
+
+    @answer = first_num + second_num
+    puts "What is #{first_num} + #{second_num}"
+  end
+
 end
